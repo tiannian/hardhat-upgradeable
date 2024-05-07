83c9a012495b279aa2d19431548176dc1e7632fd
tiannian <dtiannian@gmail.com>
2024-05-08 00:14:55 +0800

    add upgrade script

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,12 @@ require("@openzeppelin/hardhat-upgrades");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      ],
+    },
+  },
 };
